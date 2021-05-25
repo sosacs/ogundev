95b05215acb90b32bb4b86d015d15127ce8c8f0a
Cristian <scristiansosa@gmail.com>
2021-05-25 02:55:28 -0300

    app base

---
+++ ogun-app/src/OgunApp.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const OgunApp = () => {
+    return (
+        <div>
+            <h1>OgunApp</h1>
+        </div>
+    )
+}
+
---
+++ ogun-app/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { OgunApp } from './OgunApp';
+
+
+ReactDOM.render(
+    <OgunApp />,
+  document.getElementById('root')
+);
